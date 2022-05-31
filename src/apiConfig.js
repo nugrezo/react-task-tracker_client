5d3955c1863181e9ca271d2554c51f3df16b3c2a
Ergun Oz <ergun.oz82@gmail.com>
2022-05-30 22:03:06 -0400

    cors error was resolved adding 127.0.0.1:

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,10 +1,10 @@
 let apiUrl
 const apiUrls = {
   production: 'https://aqueous-atoll-85096.herokuapp.com',
-  development: 'http://localhost:4741'
+  development: 'http://127.0.0.1:4741'
 }
 
-if (window.location.hostname === 'localhost') {
+if (window.location.hostname === '127.0.0.1:') {
   apiUrl = apiUrls.development
 } else {
   apiUrl = apiUrls.production
